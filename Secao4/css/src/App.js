92f7f0d7a11160391c57e58215532f4705ad39ed
Tiago <tiagovictordeoliveira@gmail.com>
2023-04-25 23:42:48 -0300

    Iniciando seçao 4

--- Secao4/css/src/App.js
+++ Secao4/css/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import MyComponent from './components/MyComponent';
 
 function App() {
   return (
     <div className="App">
       <h1>React com CSS</h1>
+      <div>
+        <MyComponent />
+      </div>
+      <p>Este paragrafo é do App.js</p>
     </div>
   );
 }
---
+++ Secao4/css/src/components/MyComponent.js
@@ -0,0 +1,13 @@
+import './MyComponent.css'
+
+const MyComponent = () => {
+    return(
+        <div>
+            <h1>Css de componente</h1>
+            <p>Este é um paragrafo</p>
+            <p className="MyComponentParagraph" >paragrafo com css atribuido a class</p>
+        </div>
+    )
+}
+
+export default MyComponent;
