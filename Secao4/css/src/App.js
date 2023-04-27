d8e92ced956b7c53b8017ff24eb715c32d2dbb1b
Tiago <tiagovictordeoliveira@gmail.com>
2023-04-27 00:11:55 -0300

    Continuação do módulo 4

--- Secao4/css/src/App.js
+++ Secao4/css/src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
 import MyComponent from './components/MyComponent';
+import {useState} from 'react';
 
 function App() {
+  const n = 15;
+  const [name] = useState("Tiago");
+  const redTitle = false;
+
   return (
     <div className="App">
       <h1>React com CSS</h1>
@@ -9,6 +14,12 @@ function App() {
         <MyComponent />
       </div>
       <p>Este paragrafo é do App.js</p>
+      <p style={{color: "blue", padding:"25px", borderTop:"2px solid red", backgroundColor:"green"}} >Este elemento é tratado com css inline</p>
+      <h2 style={n > 10 ? ({color:"gray"}) : ({color:"pink"})}>Css dinâmico</h2>
+
+      <h2 style={name === "Tiago" ? ({color:"yellow"}) : null}>Css dinâmico</h2>
+
+      <h2 className={redTitle ? "redTitle" : "title"}>Classe dinâmica</h2>
     </div>
   );
 }
