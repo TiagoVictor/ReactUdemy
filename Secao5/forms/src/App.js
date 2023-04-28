8797d0b91553f6b26acffe3522c791b7b1379d97
Tiago <tiagovictordeoliveira@gmail.com>
2023-04-28 20:58:55 -0300

    Criação do projeto da seção 5 + primeira aula

---
+++ Secao5/forms/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import MyForm from './components/MyForms';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Forms</h2>
+      <MyForm />
+    </div>
+  );
+}
+
+export default App;
