27d7d3b7ebca2d212d72a17a6a2d1ac39928ce1b
Tiago <tiagovictordeoliveira@gmail.com>
2023-04-29 14:31:00 -0300

    andamento do projeto secret word

--- Secao6/secretword/src/App.js
+++ Secao6/secretword/src/App.js
@@ -15,9 +15,28 @@ function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
 
-  console.log(words);
+  const [pickedWord, setPickedWord] = useState("");
+  const [pickedCategory, setPickedCategory] = useState("");
+  const [letters, setLetters] = useState([]);
+
+  const pickWordAndLetter = () => {
+    const categories = Object.keys(words);
+    const category = categories[Math.floor(Math.random() * Object.keys(categories).length)]
+    const word = words[category][Math.floor(Math.random() * words[category].length)]
+
+    return {word, category};
+  }
 
   const startGame = () => {
+    const {word, category} = pickWordAndLetter();
+
+    let wordLetters = word.split("");
+    wordLetters = wordLetters.map((l) => l.toLowerCase());
+
+    setPickedWord(word);
+    setPickedCategory(category);
+    setLetters(wordLetters);
+
     setGameStage(stages[1].name);
   }
 
--- Secao6/secretword/src/components/Game.js
+++ Secao6/secretword/src/components/Game.js
@@ -2,9 +2,30 @@ import './Game.css'
 
 const Game = ({ verifyLetter }) => {
     return(
-        <div>
-            <h1>Game</h1>
-            <button onClick={verifyLetter}>Mudar estagio</button>
+        <div className='game'>
+            <p className='point'>
+                <span>Pontuação: 0</span>
+            </p>
+            <h1>Adivinhe a palavra:</h1>
+            <h3 className='tip'>
+                Dica sobre a palavra: <span>Dica...</span>
+            </h3>
+            <div className='wordContainer'>
+                <span className='letter'>A</span>
+                <span className='blankSquare'></span>
+            </div>
+            <div className='letterContainer'>
+                <p>Tente adivinhar uma letra da palavra:</p>
+                <form>
+                    <input type='text' name='letter' maxLength="1" required />
+                    <button>Jogar!</button>
+                </form>
+            </div>
+            <div className='wrongLettersContainer'>
+                <p>Letras já utilizadas:</p>
+                <span>A,</span>
+                <span>B,</span>
+            </div>
         </div>
     )
 }
