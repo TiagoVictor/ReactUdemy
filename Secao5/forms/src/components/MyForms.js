b7e851358e5a9142cc08ca16f6577b669202cc22
Tiago <tiagovictordeoliveira@gmail.com>
2023-04-28 21:24:02 -0300

    Manipulação de dados com useState

--- Secao5/forms/src/components/MyForms.js
+++ Secao5/forms/src/components/MyForms.js
@@ -1,12 +1,22 @@
 import './MyForm.css'
+import {useState} from 'react'
 
 const MyForm = () => {
+    const [name, setName] = useState();
+    const [email, setEmail] = useState();
+
+    const handleName = (e) => {
+        setName(e.target.value)
+    }
+    
+    console.log(name);
+
     return(
         <div>
             <form>
                 <div>
                     <label htmlFor="name">Nome:</label>
-                    <input type="text" name="name" placeholder="Digite seu nome"></input>
+                    <input type="text" name="name" placeholder="Digite seu nome" onChange={handleName}></input>
                 </div>
                 <label>
                     <span>E-mail:</span>
