a6ce1736466596c800ac1ca070970e78d960c6f9
Tiago <tiagovictordeoliveira@gmail.com>
2023-04-28 21:07:06 -0300

    Aula dois, label envolvendo a tag de input

--- Secao5/forms/src/components/MyForms.js
+++ Secao5/forms/src/components/MyForms.js
@@ -8,6 +8,10 @@ const MyForm = () => {
                     <label htmlFor="name">Nome:</label>
                     <input type="text" name="name" placeholder="Digite seu nome"></input>
                 </div>
+                <label>
+                    <span>E-mail:</span>
+                    <input type="text" name="email" placeholder="Digite seu e-mail"></input>
+                </label>
                 <input type="submit" value="Enviar"/>
             </form>
         </div>
